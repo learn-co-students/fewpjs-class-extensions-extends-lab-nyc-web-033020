9b483e99fd4ce31b5e21e550f2f06771b07204fa
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-05-25 22:25:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,87 @@
 // Your code here
+class Polygon {
+  constructor (sides) {
+    this.sides = sides
+  }
+
+  get countSides(){
+    return this.sides.length
+  } 
+
+  get perimeter(){
+    return this.sides.reduce(function(total, element){
+      return total + element
+    }, 0)
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid(){
+    return ((this.sides[0] + this.sides[1]) > this.sides[2] && (this.sides[2] + this.sides[0]) > this.sides[1] && (this.sides[1] + this.sides[2]) > this.sides[0])
+  }
+}
+
+class Square extends Polygon {
+  
+  get isValid(){
+    const side1 = this.sides[0];
+    const side2 = this.sides[1];
+    const side3 = this.sides[2];
+    const side4 = this.sides[3]; 
+    return (side1 === side2 && side1 === side3 && side1 === side4)
+  }
+  get area(){
+    return this.sides[0] * this.sides[0]
+  }
+}
+
+// class Polygon {
+//   constructor(sides) {
+//     this.sides = sides
+//     this.count = this.sides.length
+//   }
+
+//   get countSides() {
+//     return this.sides.length
+//   }
+
+//   get perimeter() {
+//     if (!Array.isArray(this.sides)) return;
+//     let sum = 0;
+//     for (var int of this.sides) {
+//       sum += int
+//     }
+//     return sum
+//   }
+// }
+
+// class Triangle extends Polygon {
+
+//   get isValid() {
+//     if (!Array.isArray(this.sides)) return;
+//     if (this.count !== 3) return;
+//     let side1 = this.sides[0]
+//     let side2 = this.sides[1]
+//     let side3 = this.sides[2]
+//     return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+//   }
+// }
+
+// class Square extends Polygon {
+
+//   get isValid() {
+//     if (!Array.isArray(this.sides)) return;
+//     if (this.count !== 4) return;
+//     let side1 = this.sides[0]
+//     let side2 = this.sides[1]
+//     let side3 = this.sides[2]
+//     let side4 = this.sides[3]
+//     return ((side1 === side2) && (side1 === side3) && (side1 === side4))
+//   }
+
+//   get area() {
+//     if (!Array.isArray(this.sides)) return;
+//     if (this.count !== 4) return;
+//     return this.sides[0] * this.sides[0]
+//   }
+// }
